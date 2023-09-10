04e9182d15ba2e6b06c7f3edf388a7dbe25f3ed6
perfectsoftware <perfectsoftware.in@gmail.com>
2023-09-10 10:33:02 -0700

    initial commit

---
+++ src/test/java/com/service/godash/GoDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.godash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
