c0263f0e5fc6329cfd932854d0d62261dae2d535
vsing246 <vassudha_singh@optum.com>
2023-11-02 13:17:58 +0530

    comment

--- src/main/java/com/service/godash/repository/SampleRequestRepo.java
+++ src/main/java/com/service/godash/repository/SampleRequestRepo.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SampleRequestRepo extends JpaRepository<Sample,Integer> {
 
-    Sample findBySrNo(String SrNo);
+//    Sample findBySrNo(String SrNo);
 }
